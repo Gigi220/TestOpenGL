d86a9dfd395b3cf7885de59bd52a112f3d49e4a0
Vlad.Myshenko <gigi.official.uk@gmail.com>
2020-12-15 00:46:50 +0200

    ModelMatrix transformation was fixed

--- src/Visual/Transform.h
+++ src/Visual/Transform.h
@@ -32,6 +32,7 @@ namespace Visual
 		void SetModelMatrix(const glm::mat4x4& matrix) { _matrix = matrix; }
 		void OnModelMatrixChanged()
 		{ 
+			_matrix = glm::mat4x4(1.0f);
 			_matrix = glm::translate(_matrix, _pos);
 			_matrix = glm::rotate(_matrix, glm::radians(_rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
 			_matrix = glm::rotate(_matrix, glm::radians(_rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
